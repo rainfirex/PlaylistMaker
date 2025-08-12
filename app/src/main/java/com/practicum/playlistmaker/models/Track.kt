210e03d58964c85d10d02b6d25f1a1bf9d9060fb
Александр <rainfire491@gmail.com>
2025-08-12 14:02:40 +1100

    Задача спринта №10 (clear comments)

--- app/src/main/java/com/practicum/playlistmaker/models/Track.kt
+++ app/src/main/java/com/practicum/playlistmaker/models/Track.kt
@@ -1,8 +1,3 @@
 package com.practicum.playlistmaker.models
 
 data class Track (val trackName: String, val artistName: String, val trackTime: String, val artworkUrl100: String)
-
-//trackName: String // Название композиции
-//artistName: String // Имя исполнителя
-//trackTime: String // Продолжительность трека
-//artworkUrl100: String // Ссылка на изображение обложки
